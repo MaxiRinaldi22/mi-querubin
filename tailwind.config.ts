b1dcd7ef26eede2b4c2ea27708bdf0c65e3fe6dd
Maxi <Mrinaldi2204@gmail.com>
2025-01-20 10:33:26 -0300

    Add tailwindcss-animate plugin and update tailwind configuration; include tailwind.config.ts in TypeScript compilation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 export default {
   darkMode: ["class"],
@@ -10,76 +11,77 @@ export default {
   theme: {
     extend: {
       textShadow: {
-        default: '0 2px 4px rgba(0, 0, 0, 0.1)',
-        sm: '0 1px 2px rgba(0, 0, 0, 0.5)',
-        lg: '0 33px 36px rgba(0, 0, 0, 1)',
+        default: "0 2px 4px rgba(0, 0, 0, 0.1)",
+        sm: "0 1px 2px rgba(0, 0, 0, 0.5)",
+        lg: "0 33px 36px rgba(0, 0, 0, 1)",
       },
       colors: {
         primaryColor: "#74ffeb",
         secondaryColor: "#003b73",
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
         card: {
-          DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))',
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
         },
         popover: {
-          DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))',
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
         },
         primary: {
-          DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))',
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
-          DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))',
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
         },
         muted: {
-          DEFAULT: 'hsl(var(--muted))',
-          foreground: 'hsl(var(--muted-foreground))',
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
         },
         accent: {
-          DEFAULT: 'hsl(var(--accent))',
-          foreground: 'hsl(var(--accent-foreground))',
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
         },
         destructive: {
-          DEFAULT: 'hsl(var(--destructive))',
-          foreground: 'hsl(var(--destructive-foreground))',
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
         },
-        border: 'hsl(var(--border))',
-        input: 'hsl(var(--input))',
-        ring: 'hsl(var(--ring))',
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
         chart: {
-          '1': 'hsl(var(--chart-1))',
-          '2': 'hsl(var(--chart-2))',
-          '3': 'hsl(var(--chart-3))',
-          '4': 'hsl(var(--chart-4))',
-          '5': 'hsl(var(--chart-5))',
+          "1": "hsl(var(--chart-1))",
+          "2": "hsl(var(--chart-2))",
+          "3": "hsl(var(--chart-3))",
+          "4": "hsl(var(--chart-4))",
+          "5": "hsl(var(--chart-5))",
         },
       },
       borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
   plugins: [
-    require("tailwindcss-animate"),
+    tailwindcssAnimate,
+    // @ts-expect-error : expected any
     function ({ addUtilities }) {
       const newUtilities = {
-        '.text-shadow-sm': {
-          textShadow: '0 1px 2px rgba(0, 0, 0, 0.5)',
+        ".text-shadow-sm": {
+          textShadow: "0 1px 2px rgba(0, 0, 0, 0.5)",
         },
-        '.text-shadow': {
-          textShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+        ".text-shadow": {
+          textShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
         },
-        '.text-shadow-lg': {
-          textShadow: '0 3px 6px rgba(0, 0, 0, 0.2)',
+        ".text-shadow-lg": {
+          textShadow: "0 3px 6px rgba(0, 0, 0, 0.2)",
         },
       };
-      addUtilities(newUtilities, ['responsive', 'hover']);
+      addUtilities(newUtilities, ["responsive", "hover"]);
     },
   ],
 } satisfies Config;
